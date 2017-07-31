2a9c926af7f01a35aa3bd90270974fa1f7dc576f
yangxiaozhen0505 <736453008@qq.com>
2017-07-31 20:21:49 +0800

    myshop

--- mock/sliders.js
+++ mock/sliders.js
@@ -1,7 +1,7 @@
 
 module.exports =
   [
-    '//image4.suning.cn/uimg/cms/img/149788424640147846.jpg',
-    '//image2.suning.cn/uimg/cms/img/149787421566598471.jpg',
-    '//image2.suning.cn/uimg/cms/img/149787428742824667.jpg'
+    'https://img12.360buyimg.com/da/jfs/t6484/289/2487154863/134028/305095ca/5962efb9Na779e064.jpg!q80.webp',
+    'https://img30.360buyimg.com/da/jfs/t5860/136/6981198465/287834/1c165874/596daebfNeea4cdda.jpg!q80.webp',
+    'https://img14.360buyimg.com/da/jfs/t6043/216/6383075299/80688/b256cbae/5975692dN71a725b3.jpg!q80.webp'
   ];
